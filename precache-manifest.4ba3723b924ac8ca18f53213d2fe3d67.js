42c9f6bb4fcc0a01d32ee5eaed03c8c1f83be5f6
Darshan <darshan@Darshans-MacBook-Pro.local>
2021-07-16 21:29:32 +0530

    updated

---
+++ precache-manifest.4ba3723b924ac8ca18f53213d2fe3d67.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeeb0289d18f7f308d8d2bb24b8c327a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f06c8120663e2322615c",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "ee42610210e55a15286c",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "6d8b9d4223c5e362818a",
+    "url": "/static/css/main.0fdb9f58.chunk.css"
+  },
+  {
+    "revision": "7fa369580e44b4e10980",
+    "url": "/static/js/2.f5c1bf33.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.f5c1bf33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06c8120663e2322615c",
+    "url": "/static/js/3.1fb5f720.chunk.js"
+  },
+  {
+    "revision": "ee42610210e55a15286c",
+    "url": "/static/js/4.ee8adacf.chunk.js"
+  },
+  {
+    "revision": "6d8b9d4223c5e362818a",
+    "url": "/static/js/main.af740a30.chunk.js"
+  },
+  {
+    "revision": "f2d00524ec71a9d0972b",
+    "url": "/static/js/runtime-main.d1a48c34.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "113a23999d883fde596c64c3abdc48b4",
+    "url": "/static/media/ICECA logo.113a2399.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "af49c6a19e334d9e6ff8e5b0e9951fb8",
+    "url": "/static/media/Zee5.af49c6a1.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "37568bb001f6ea3bd55c78d225b1a573",
+    "url": "/static/media/hug.37568bb0.jpg"
+  },
+  {
+    "revision": "45a15e1159a7d76808dc45af3fada860",
+    "url": "/static/media/kjsce.45a15e11.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4c534e97c0ed85671996b8e3a85facd2",
+    "url": "/static/media/mumbai-university.4c534e97.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "cc21f86fd30581c61195f9baaf06bb51",
+    "url": "/static/media/udacity.cc21f86f.jpeg"
+  },
+  {
+    "revision": "a85a0ee0745c1e328326cca89074203a",
+    "url": "/static/media/xilytica.a85a0ee0.jpg"
+  }
+]);
